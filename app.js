2412091fce775a390e12488721bf55535a99fd77
HardlySalty <118101156+HardlySalty@users.noreply.github.com>
2023-01-28 14:12:22 -0700

    Added the upgradeFormula and math for Microwave

--- app.js
+++ app.js
@@ -1,22 +1,80 @@
 // STUB upgrades
 let pointerUpgrades = [
-  {name: "Hand", quantity: 0, clickCount: 1,cost: 200,
-  costAddition: 200, quanityCheck: [6, 16, 26, 41, 71],
-  costCheck: [1000, 2500, 5000, 10000, 15000],
-  clickCheck: [2, 5, 10, 50, 100]},
+  {name: "Hand", quantity: 0, clickCount: 1, cost: 200, costAddition: 200,
+  upgradeFormula: [
+    {
+      startQuantity: 5,
+      costPerItem: 1000,
+      cupcakePerClick: 2
+    },
+    {
+      startQuantity: 15,
+      costPerItem: 2500,
+      cupcakePerClick: 5
+    },
+    {
+      startQuantity: 25,
+      costPerItem: 5000,
+      cupcakePerClick: 10
+    },
+    {
+      startQuantity: 40,
+      costPerItem: 10000,
+      cupcakePerClick: 50
+    },
+    {
+      startQuantity: 70,
+      costPerItem: 15000,
+      cupcakePerClick: 100
+    },
+  ]},
 
-  {name: "Oven Mitt", quantity: 0, clickMultiplier: .1, cost: 5000,costAddition: 5000,
-    quanityCheck: [6, 16, 26, 41, 71],
+  {name: "Oven Mitt", quantity: 0, clickMultiplier: .1, cost: 5000, costAddition: 5000,
+    quantityCheck: [6, 16, 26, 41, 71],
     costCheck: [10000, 20000, 30000, 50000, 80000],
     clickCheck: [.2, .3, .4, .5, .6]},
 ]
 
 let automaticUpgrades = [
-  {name: "Microwave", quantity: 0, clickCount: 1, cost: 40, costAddition: 40, quanityCheck: [16, 31, 46, 61, 71],costCheck: [100, 500, 1500, 3000, 5000],clickCheck: [5, 15, 20, 50, 70]},
+  {name: "Microwave", quantity: 0, clickCount: 1, cost: 40, costAddition: 40,
+   upgradeFormula:[
+    {
+      startQuantity: 15,
+      costPerItem: 100,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 30,
+      costPerItem: 500,
+      cupcakePerClick: 15,
+    },
+    {
+      startQuantity: 45,
+      costPerItem: 1500,
+      cupcakePerClick: 20,
+    },
+    {
+      startQuantity: 55,
+      costPerItem: 3000,
+      cupcakePerClick: 50,
+    },
+    {
+      startQuantity: 70,
+      costPerItem: 5000,
+      cupcakePerClick: 70,
+    },
+   ]},
   {name: "Grandma", quantity: 0, clickCount: 15, cost: 300, costAddition: 300,
-  quanityCheck: [11, 26, 36, 51, 71],
+  quantityCheck: [11, 26, 36, 51, 71],
   costCheck: [500, 1500, 3000, 5000, 10000],
-  clickCheck: [5, 15, 20, 50, 70]},
+  clickCheck: [5, 15, 20, 50, 70],
+  upgradeFormula: [
+  {
+    startQuantity: 15,
+    costPerItem: 100,
+    cupcakePerClick: 5,
+  }
+  ]},
   {name: "Oven", quantity: 0, clickCount: 50, cost: 2000, costAddition: 2000,},
   {name: "Food Truck", quantity: 0, clickCount: 1000, cost: 5000, costAddition: 5000,},
   {name: "Factory", quantity: 0, clickCount: 3000, cost: 10000, costAddition: 10000,},
@@ -93,22 +151,29 @@ function updateInfo(){
   upgradesOwnedElem.innerText = `Upgrades owned: ${totalUpgrades}`
 }
 
-function checkUpgrade(upgrade, upgradesElem){
-  console.log(upgrade.quantityCheck, "a")
+function checkUpgrade(upgrade, upgradeElem){
   if(cupcakeBank >= upgrade.cost){
     upgrade.quantity++
-    if(upgrade.quanity == upgrade.quantityCheck[0]){
-      upgrade.costAddition = costCheck[0]
-      upgrade.quantityCheck.splice(0, 1)
-      upgrade.costCheck.splice(0, 1)
-      if(upgrade.quantityCheck.length == 0){
-        upgrade.quantityCheck.push(99999999999999999999999999999)
+    if(upgrade.upgradeFormula.length > 0){
+      let nextStat = upgrade.upgradeFormula[0]
+      console.log(nextStat, upgrade)
+      if(upgrade.quantity == nextStat.startQuantity){
+        console.log('yep')
+        upgrade.costAddition = nextStat.costPerItem
+        upgrade.clickCount = nextStat.cupcakePerClick
+        upgrade.upgradeFormula.splice(0, 1)
+        if(upgrade.upgradeFormula.length == 0){
+          upgrade.upgradeFormula.push({
+            startQuantity: 0,
+            costPerItem: 0,
+            cupcakePerClick: 0,
+          },)
+        }
       }
     }
     upgrade.cost += upgrade.costAddition
-    upgradesElem.title = `Cost: ${Math.ceil(upgrade.cost)}`
+    upgradeElem.title = `Cost: ${Math.ceil(upgrade.cost)}`
   }
-  console.log(upgrade, "b")
 }
 
 // SECTION EVENT LISTENERS
