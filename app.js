b3dc0557404abcccf3e5eb22d8efd9bd1111f4f8
HardlySalty <118101156+HardlySalty@users.noreply.github.com>
2023-01-28 14:22:41 -0700

    Added the math formulas for all the upgrades

--- app.js
+++ app.js
@@ -30,9 +30,33 @@ let pointerUpgrades = [
   ]},
 
   {name: "Oven Mitt", quantity: 0, clickMultiplier: .1, cost: 5000, costAddition: 5000,
-    quantityCheck: [6, 16, 26, 41, 71],
-    costCheck: [10000, 20000, 30000, 50000, 80000],
-    clickCheck: [.2, .3, .4, .5, .6]},
+  upgradeFormula: [
+    {
+      startQuantity: 5,
+      costPerItem: 10000,
+      cupcakePerClick: .2,
+    },
+    {
+      startQuantity: 15,
+      costPerItem: 20000,
+      cupcakePerClick: .3,
+    },
+    {
+      startQuantity: 25,
+      costPerItem: 30000,
+      cupcakePerClick: .4,
+    },
+    {
+      startQuantity: 40,
+      costPerItem: 50000,
+      cupcakePerClick: .5,
+    },
+    {
+      startQuantity: 70,
+      costPerItem: 80000,
+      cupcakePerClick: .6,
+    },
+  ]},
 ]
 
 let automaticUpgrades = [
@@ -65,19 +89,122 @@ let automaticUpgrades = [
     },
    ]},
   {name: "Grandma", quantity: 0, clickCount: 15, cost: 300, costAddition: 300,
-  quantityCheck: [11, 26, 36, 51, 71],
-  costCheck: [500, 1500, 3000, 5000, 10000],
-  clickCheck: [5, 15, 20, 50, 70],
   upgradeFormula: [
   {
-    startQuantity: 15,
-    costPerItem: 100,
+    startQuantity: 10,
+    costPerItem: 500,
     cupcakePerClick: 5,
-  }
+  },
+  {
+    startQuantity: 25,
+    costPerItem: 1500,
+    cupcakePerClick: 15,
+  },
+  {
+    startQuantity: 35,
+    costPerItem: 3000,
+    cupcakePerClick: 20,
+  },
+  {
+    startQuantity: 50,
+    costPerItem: 5000,
+    cupcakePerClick: 50,
+  },
+  {
+    startQuantity: 70,
+    costPerItem: 10000,
+    cupcakePerClick: 70,
+  },
   ]},
-  {name: "Oven", quantity: 0, clickCount: 50, cost: 2000, costAddition: 2000,},
-  {name: "Food Truck", quantity: 0, clickCount: 1000, cost: 5000, costAddition: 5000,},
-  {name: "Factory", quantity: 0, clickCount: 3000, cost: 10000, costAddition: 10000,},
+  {name: "Oven", quantity: 0, clickCount: 50, cost: 2000, costAddition: 2000,
+  upgradeFormula: [
+    {
+      startQuantity: 10,
+      costPerItem: 5000,
+      cupcakePerClick: 150,
+    },
+    {
+      startQuantity: 25,
+      costPerItem: 8000,
+      cupcakePerClick: 350,
+    },
+    {
+      startQuantity: 40,
+      costPerItem: 12000,
+      cupcakePerClick: 600,
+    },
+    {
+      startQuantity: 60,
+      costPerItem: 15000,
+      cupcakePerClick: 900,
+    },
+    {
+      startQuantity: 75,
+      costPerItem: 20000,
+      cupcakePerClick: 1300,
+    },
+  ]},
+  {name: "Food Truck", quantity: 0, clickCount: 1000, cost: 5000, costAddition: 5000,
+  upgradeFormula: [
+    {
+      startQuantity: 10,
+      costPerItem: 10000,
+      cupcakePerClick: 1500,
+    },
+    {
+      startQuantity: 25,
+      costPerItem: 15000,
+      cupcakePerClick: 2000,
+    },
+    {
+      startQuantity: 40,
+      costPerItem: 20000,
+      cupcakePerClick: 4000,
+    },
+    {
+      startQuantity: 50,
+      costPerItem: 30000,
+      cupcakePerClick: 6000,
+    },
+    {
+      startQuantity: 65,
+      costPerItem: 45000,
+      cupcakePerClick: 10000,
+    },
+  ]},
+  {name: "Factory", quantity: 0, clickCount: 3000, cost: 10000, costAddition: 10000,
+  upgradeFormula: [
+    {
+      startQuantity: 10,
+      costPerItem: 20000,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 25,
+      costPerItem: 50000,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 40,
+      costPerItem: 100000,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 50,
+      costPerItem: 300000,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 60,
+      costPerItem: 800000,
+      cupcakePerClick: 5,
+    },
+    {
+      startQuantity: 70,
+      costPerItem: 1500000,
+      cupcakePerClick: 5,
+    },
+  ]},
 ]
 
 // SECTION element selectors
